e5c40efab92a4758eea85215208176557a5b88ee
americo afonso <afonsoamericoafonso@gmail.com>
2021-03-01 22:28:12 -0300

    Applying HTTP Status to a resource that does not exist.

--- algafood-api/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -3,7 +3,10 @@ package com.algaworks.algafood.api.controller;
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +22,18 @@ public class CozinhaController {
     @GetMapping
     public List<Cozinha> listar() {
         return cozinhaRepository.listar();
+    }
+
+    @GetMapping("/{cozinhaId}")
+    public ResponseEntity<Cozinha> bucar(@PathVariable("cozinhaId") Long id) {
+        Cozinha cozinha = cozinhaRepository.buscar(id);
+
+        if (cozinha != null) {
+            //return ResponseEntity.status(HttpStatus.OK).build(); ~ esta linha retorna o status (200 OK) sem o payload
+            //return ResponseEntity.ok(cozinha); ~ este linha faz a mesma coisa que a linha de baixo
+            return ResponseEntity.status(HttpStatus.OK).body(cozinha);
+        }
+        //return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
     }
 }
