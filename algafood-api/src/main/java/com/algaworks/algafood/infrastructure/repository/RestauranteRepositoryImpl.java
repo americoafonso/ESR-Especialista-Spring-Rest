419db57ca1bc909ba510294afe9f7704dcc22551
americo afonso <afonsoamericoafonso@gmail.com>
2021-03-11 21:07:19 -0300

    Implementation of restaurant features.

--- algafood-api/src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
+++ algafood-api/src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -2,11 +2,14 @@ package com.algaworks.algafood.infrastructure.repository;
 
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.RestauranteRepository;
+import org.springframework.stereotype.Component;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
 import java.util.List;
 
+@Component
 public class RestauranteRepositoryImpl implements RestauranteRepository {
 
     @PersistenceContext
@@ -14,7 +17,7 @@ public class RestauranteRepositoryImpl implements RestauranteRepository {
 
     @Override
     public List<Restaurante> listar() {
-        return manager.createQuery("from Retaurante", Restaurante.class).getResultList();
+        return manager.createQuery("from Restaurante", Restaurante.class).getResultList();
     }
 
     @Override
@@ -22,11 +25,13 @@ public class RestauranteRepositoryImpl implements RestauranteRepository {
         return manager.find(Restaurante.class, id);
     }
 
+    @Transactional
     @Override
     public Restaurante salvar(Restaurante restaurante) {
         return manager.merge(restaurante);
     }
 
+    @Transactional
     @Override
     public void remover(Restaurante restaurante) {
         restaurante = buscar(restaurante.getId());
