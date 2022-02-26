7c6a03438ae9fb20319ea0246ccbe8bb2e69d703
americoafonso <americo.quim@hotmail.com>
2022-02-26 12:26:43 -0300

    Preparing the project for integration testing.

--- algafood-api/src/test/java/com/algaworks/algafood/AlgafoodApiApplicationTests.java
+++ algafood-api/src/test/java/com/algaworks/algafood/AlgafoodApiApplicationTests.java
@@ -1,13 +1,19 @@
 package com.algaworks.algafood;
 
-import org.testng.annotations.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.testng.annotations.Test;
 
+import static org.junit.Assert.assertFalse;
+
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
-class AlgafoodApiApplicationTests {
+public class AlgafoodApiApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertFalse(false);
 	}
 
 }
