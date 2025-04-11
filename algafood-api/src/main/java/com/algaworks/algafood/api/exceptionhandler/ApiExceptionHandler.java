84fb5edbe0c0602eca2568ae6b41282165de8690
americoafonso <americo.quim@hotmail.com>
2025-04-10 21:54:21 -0300

    Setting up a test bench and using @TestPropertySource.

--- algafood-api/src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -28,7 +28,6 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Locale;
 import java.util.stream.Collectors;
 
 @ControllerAdvice //~> Essa anotacao fala que todas as excecoes de todos controladores serao tratadas aqui nessa classe
